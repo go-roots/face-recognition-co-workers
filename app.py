3795824347d1c3818ad345149a9f0988230bc772
PaulAmb325 <63868715+PaulAmb325@users.noreply.github.com>
2020-11-08 15:30:57 +0100

    Fixed things

--- app.py
+++ app.py
@@ -147,5 +147,6 @@ def upload_image():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     loadImages()
